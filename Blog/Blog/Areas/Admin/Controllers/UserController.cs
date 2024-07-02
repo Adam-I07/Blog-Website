d565ebf5ad53d011697605e2b4e8c1a2b0d3e01f
Adam-I07 <90919913+Adam-I07@users.noreply.github.com>
2024-07-02 17:03:57 +0100

    Added CSS and Admin
    Added css theme for the front end and also added an admin section and the css for the admin dashboard

---
+++ Blog/Blog/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+ 
