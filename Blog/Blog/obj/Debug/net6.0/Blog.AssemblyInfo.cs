3d540a2fcdc67a91c537fb68b1e399dcd33b075a
Adam-I07 <90919913+Adam-I07@users.noreply.github.com>
2024-07-03 16:34:57 +0100

    remove .vs files

--- Blog/Blog/obj/Debug/net6.0/Blog.AssemblyInfo.cs
+++ Blog/Blog/obj/Debug/net6.0/Blog.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d565ebf5ad53d011697605e2b4e8c1a2b0d3e01f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+112ab8fed9f7daccc6f2ede808dc2f9e5225401e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
