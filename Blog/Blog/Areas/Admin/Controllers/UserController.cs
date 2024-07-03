d5c98fed0b69f179f525c10953b0b7087a76d062
Adam-I07 <90919913+Adam-I07@users.noreply.github.com>
2024-07-03 17:02:59 +0100

    Added logout functionality

--- Blog/Blog/Areas/Admin/Controllers/UserController.cs
+++ Blog/Blog/Areas/Admin/Controllers/UserController.cs
@@ -26,7 +26,11 @@ namespace Blog.Areas.Admin.Controllers
 
         [HttpGet("Login")]
         public IActionResult Login() {
-            return View(new LoginVM());
+            if(!HttpContext.User.Identity.IsAuthenticated) {
+                return View(new LoginVM());
+            }
+            return RedirectToAction("Index", "User", new {area = "Admin"});
+            
         }
 
         [HttpPost("Login")]
@@ -50,6 +54,13 @@ namespace Blog.Areas.Admin.Controllers
             await _signInManager.PasswordSignInAsync(vm.Username, vm.Password, vm.RememberMe, true);
             _notification.Success("Login Successful");
             return RedirectToAction("Index", "User", new {area="Admin"});
+        }
+
+        [HttpPost]
+        public IActionResult Logout() {
+            _signInManager.SignOutAsync();
+            _notification.Success("Logged out!");
+            return RedirectToAction("Index", "Home", new {area = ""});
         }
     }
 }
