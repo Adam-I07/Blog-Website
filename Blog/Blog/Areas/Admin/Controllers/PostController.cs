717b9111a428df66b642e64564b08fef3a052985
Adam-I07 <90919913+Adam-I07@users.noreply.github.com>
2024-07-10 15:38:48 +0100

    Added Post and Delete function
    Added posts menu for author and admin to view all the posts made for admin. for author to just view their posts made. I have also added functionality to let the admin delete any post they want and the author to delete any blog post made by them

--- Blog/Blog/Areas/Admin/Controllers/PostController.cs
+++ Blog/Blog/Areas/Admin/Controllers/PostController.cs
@@ -1,6 +1,7 @@
 ﻿using AspNetCoreHero.ToastNotification.Abstractions;
 using Blog.Data;
 using Blog.Models;
+using Blog.Utilities;
 using Blog.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -26,9 +27,29 @@ namespace Blog.Areas.Admin.Controllers
             _userManager = userManager;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var listOfPosts = new List<Post>();
+            var loggedInUser  = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity!.Name);
+            var loggedInUserRole = await _userManager.GetRolesAsync(loggedInUser!);
+            if (loggedInUserRole[0] == WebsiteRoles.WebsiteAdmin)
+            {
+                listOfPosts = await _context.Posts!.Include(x => x.ApplicationUser).ToListAsync();
+            }
+            else
+            {
+                listOfPosts = await _context.Posts!.Include(x => x.ApplicationUser).Where(x=>x.ApplicationUser!.Id==loggedInUser!.Id).ToListAsync();
+            }
+            var listOfPostsVM = listOfPosts.Select(x => new PostVM()
+            {
+                Id = x.Id,
+                Title = x.Title,
+                CreatedDate = x.CreatedDate,
+                ThumbnailURL = x.ThumbnailUrl,
+                AuthorName = x.ApplicationUser!.FirstName + " " + x.ApplicationUser.LastName
+            }).ToList();
+            return View(listOfPostsVM);
         }
 
         [HttpGet]
@@ -69,6 +90,24 @@ namespace Blog.Areas.Admin.Controllers
             await _context.SaveChangesAsync();
             _notification.Success("Post Created Successfuly");
             return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var post = await _context.Posts!.FirstOrDefaultAsync(x => x.Id == id);
+            var loggedInUser = await _userManager.Users.FirstOrDefaultAsync(x=>x.UserName == User.Identity!.Name);
+            var loggedInUserRole = await _userManager.GetRolesAsync(loggedInUser!);
+
+            if (loggedInUserRole[0] == WebsiteRoles.WebsiteAdmin || loggedInUser?.Id == post?.ApplicationUserId)
+            {
+                _context.Posts!.Remove(post!);
+                await _context.SaveChangesAsync();
+                _notification.Success("Post Deleted Successfully");
+                return RedirectToAction("Index", "Post", new { area = "Admin" });
+            }
+
+            return View();
         }
 
         private string UploadImage(IFormFile file)
