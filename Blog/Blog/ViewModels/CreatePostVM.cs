628b1291f8f1fc1b4f3e9590d39aa377b89b6102
Adam-I07 <90919913+Adam-I07@users.noreply.github.com>
2024-07-11 15:09:43 +0100

    Added Edit function
    Add3ed funcitonality that will allow th euser that has posted a blog post to go back and edit their own post. I also added functiionlaity tthat will allow the admin to edit any users existing blog posts.

--- Blog/Blog/ViewModels/CreatePostVM.cs
+++ Blog/Blog/ViewModels/CreatePostVM.cs
@@ -12,6 +12,6 @@ namespace Blog.ViewModels
         public string? ApplicationUserId { get; set; }
         public string? Description { get; set; }
         public string? ThumbnailUrl { get; set; }
-        public IFormFile Thumbnail { get; set; }
+        public IFormFile? Thumbnail { get; set; }
     }
 }
