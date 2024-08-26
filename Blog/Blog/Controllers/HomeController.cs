d5f88ce5c2eeec278f2fdcaf931336e00c47ea73
Adam-I07 <90919913+Adam-I07@users.noreply.github.com>
2024-08-26 15:07:00 +0100

    Updated Main index page
    I updated the main page to now reflect changes made by the setting page and also updated the blog posts displayed to only be the ones made by the user.

--- Blog/Blog/Controllers/HomeController.cs
+++ Blog/Blog/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using Blog.Models;
+﻿using Blog.Data;
+using Blog.Models;
+using Blog.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Blog.Controllers
@@ -7,13 +10,21 @@ namespace Blog.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger) {
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context) {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index() {
-            return View();
+            var vm = new HomeVM();
+            var setting = _context.Settings!.ToList();
+            vm.Title = setting[0].Title;
+            vm.ShortDescription = setting[0].ShortDescription;
+            vm.ThumbnailUrl = setting[0].ThumbnailUrl;
+            vm.Posts = _context.Posts!.Include(x=>x.ApplicationUser).ToList();
+            return View(vm);
         }
 
         public IActionResult Privacy() {
